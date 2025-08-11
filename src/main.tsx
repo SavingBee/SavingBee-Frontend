96defbc0c3338efc6662efc54b5a7480191f2238
hyunkyung <yellowbowl21@gmail.com>
2025-08-11 18:56:52 +0900

    :sparkles:feat: MSW setup - 기본 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/**
+ * .env --- VITE_USE_MOCK 값으로 제어
+ */
+if (import.meta.env.VITE_USE_MOCK !== "0") {
+  const { worker } = await import("./mocks/browser");
+  await worker.start({
+    serviceWorker: { url: "/mockServiceWorker.js" }, // public 내에 위치
+    //정의 안 된 API 처리 -> 실제 네트워크로
+    // onUnhandledRequest: 'bypass',
+  });
+}
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
