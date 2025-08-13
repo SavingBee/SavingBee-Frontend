fd8432cbf94c0a6ea16bdaa2f1a7261c0cde3bd7
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-08-14 07:16:49 +0900

    💄style: 스타일 수정 및 불필요한 단위 표시 코드 제거

--- src/components/search/dropdown/AmountForm.tsx
+++ src/components/search/dropdown/AmountForm.tsx
@@ -19,25 +19,18 @@ export default function AmountForm({ aFilter, onApply }: AmountFormProps) {
   };
 
   return (
-    <div className="flex items-center gap-2 w-[360px] border p-4 rounded-md">
-      <div className="relative flex-1">
-        <InputField1
-          type="text"
-          variant="sm"
-          inputClassName="border-graye5 rounded-md py-3 outline-none pr-10"
-          placeholder={
-            aFilter.placeholder ??
-            (aFilter.unit ? `금액 입력 (${aFilter.unit})` : "금액 입력")
-          }
-          value={display}
-          onChange={handleChange}
-        />
-        {aFilter.unit && (
-          <span className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 text-sm">
-            {aFilter.unit}
-          </span>
-        )}
-      </div>
+    <div className="flex items-center gap-2 border p-4 rounded-md">
+      <InputField1
+        type="text"
+        variant="sm"
+        inputClassName="border-graye5 rounded-md py-3 outline-none pr-10"
+        placeholder={
+          aFilter.placeholder ??
+          (aFilter.unit ? `금액 입력 (${aFilter.unit})` : "금액 입력")
+        }
+        value={display}
+        onChange={handleChange}
+      />
 
       <Button
         type="button"
