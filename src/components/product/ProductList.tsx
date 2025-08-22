4d9f18a39962bc93ea2c1660067353bc356a6762
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-08-23 08:27:21 +0900

    ✨ feat: ProductList 컴포넌트에 variant 및 selectedIds props 추가

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,23 +1,31 @@
-import React from "react";
 import ProductListItem, { ProductListItemProps } from "./ProductListItem";
-
 type Item = ProductListItemProps & { id: string };
 
 type ProductListProps = {
   items: Item[];
   onCompare?: (id: string) => void;
   listClassName?: string;
+  variant?: "search" | "compare";
+  selectedIds?: string[];
 };
 
 const ProductList: React.FC<ProductListProps> = ({
   items,
   onCompare,
   listClassName,
+  variant = "search",
+  selectedIds = [],
 }) => {
   return (
-    <ul className={["space-y-3  ", listClassName].filter(Boolean).join(" ")}>
+    <ul className={["space-y-3", listClassName].filter(Boolean).join(" ")}>
       {items.map(({ id, ...rest }) => (
-        <ProductListItem key={id} {...rest} onCompare={() => onCompare?.(id)} />
+        <ProductListItem
+          key={id}
+          {...rest}
+          variant={variant}
+          selected={selectedIds.includes(id)}
+          onCompare={() => onCompare?.(id)}
+        />
       ))}
     </ul>
   );
