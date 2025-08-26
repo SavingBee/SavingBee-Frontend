0237241b8e988bab9aae8acbd9fefb61705581ad
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-08-27 00:05:56 +0900

    ✨ feat: 마이페이지 알림 버튼 및 모달 추가

--- src/components/mypage/MypageContainer.tsx
+++ src/components/mypage/MypageContainer.tsx
@@ -3,38 +3,51 @@ import PageHeader from "../common/pageHeader/PageHeader";
 import UserHeader from "./UserHeader";
 import Button from "../common/button/Button";
 import UserEditModal from "./UserEditModal";
+import AlertModal from "./alert/AlertModal";
 
 const MypageContainer = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
 
-    return (
-        <div>
-            <PageHeader
-                title="마이페이지"
-                breadcrumb={[
-                    { label: "홈", to: "/" },
-                    { label: "마이페이지", to: "/mypage" }
-                ]}
-            />
-            <div className="flex justify-end mb-2">
-                <Button
-                    type="button"
-                    onClick={() => setIsOpen(true)}
-                    variant="sm"
-                    styleVariant="border"
-                    className="font-medium"
-                >
-                    회원정보수정
-                </Button>
-                <UserEditModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
-            </div>
-            <UserHeader />
-            <div className="mt-9">
-                <strong className="gmarket font-bold text-2xl">
-                    Nickname을 위한 <span className="text-primary">추천 상품</span>
-                </strong>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <PageHeader
+        title="마이페이지"
+        breadcrumb={[
+          { label: "홈", to: "/" },
+          { label: "마이페이지", to: "/mypage" },
+        ]}
+      />
+      <div className="flex justify-end mb-2 gap-2">
+        <Button
+          type="button"
+          variant="sm"
+          styleVariant="border"
+          onClick={() => setOpenAlert((prev) => !prev)}
+        >
+          알림 설정
+        </Button>
+        {openAlert && (
+          <AlertModal closeAlert={() => setOpenAlert((prev) => !prev)} />
+        )}
+        <Button
+          type="button"
+          onClick={() => setIsOpen(true)}
+          variant="sm"
+          styleVariant="border"
+          className="font-medium"
+        >
+          회원정보수정
+        </Button>
+        <UserEditModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      </div>
+      <UserHeader />
+      <div className="mt-9">
+        <strong className="gmarket font-bold text-2xl">
+          Nickname을 위한 <span className="text-primary">추천 상품</span>
+        </strong>
+      </div>
+    </div>
+  );
+};
 export default MypageContainer;
