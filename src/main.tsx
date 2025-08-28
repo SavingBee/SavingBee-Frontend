19390408a55e651210a51a948e4433cee5bb7f5c
tiyu25 <tiyuuuu9@gmail.com>
2025-08-28 22:16:02 +0900

    ✨ feat: 로그인 API 연동

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ if (import.meta.env.VITE_USE_MOCK !== "0") {
   await worker.start({
     serviceWorker: { url: "/mockServiceWorker.js" }, // public 내에 위치
     //정의 안 된 API 처리 -> 실제 네트워크로
-    // onUnhandledRequest: 'bypass',
+    onUnhandledRequest: 'bypass',
   });
 }
 
