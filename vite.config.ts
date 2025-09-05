326dcd519324ba181a7a9e0e334b0b9b28fb223f
tiyu25 <tiyuuuu9@gmail.com>
2025-09-05 19:58:09 +0900

    🔧 config: vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,12 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      '/': {
-        target: 'http://34.64.73.53:8080',
-        changeOrigin: true,
-      },
-    },
-  }
 });
